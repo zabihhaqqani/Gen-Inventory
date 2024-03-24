0deb9aea09838587e304149d7504b3eb423bd6ec
Zabih <mdzabihhaqqani@gmail.com>
2024-03-24 14:40:10 +0530

    Update Sales.js

--- src/Redux/Actions/Sales.js
+++ src/Redux/Actions/Sales.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiEndpoint =
-  "https://inventory-management.zabihhaqqani1.repl.co/api/sales";
+  "https://7f9a6a45-68a8-472d-ac44-ff704d9ded5c-00-2n8rzw8p3sn1c.pike.replit.dev/api/sales";
 
 export const fetchSales= () => async (dispatch) => {
   try {
